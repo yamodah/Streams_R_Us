ecba2d75256397a2d40de29feb65b9ac17c7592d
yamodah <yassinedahlek@gmail.com>
2022-04-09 19:19:45 -0400

    basic config to grab stream data

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import "./home.css"
 export default function Home() {
     const [search, setSearch]= useState("")
     const [data, setData]= useState()
+    const getStreams =async()=>{
+        await axios.get(`http://localhost:5000/all?stream=${search.searchFor}`)
+            .then((res)=>{console.log(res)})
+            .catch(console.error)
+    }
     const handleSubmit=(e)=>{
         e.preventDefault()
-
+        console.log(search)
+        getStreams().then(()=>{console.log(data)})
     }
     const handleChange = ({ target }) => {
         setSearch({
@@ -19,10 +26,11 @@ export default function Home() {
     <form onSubmit={handleSubmit}>
         <label>
           Desired Live Event:
-          <input type="text" value={search.searchFor} onChange={handleChange} />
+          <input type="text" name="searchFor" value={search.searchFor} onChange={handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      {/* <h4>{data}</h4> */}
 </div>
   )
 }

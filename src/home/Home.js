6ebba3636451b365f8710503eb9b005547a06c72
yamodah <yassinedahlek@gmail.com>
2022-04-09 16:34:49 -0400

    removed bootstrap just using regular form now

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Form , Button} from 'react-bootstrap'
+import React, { useState } from 'react'
+import "./home.css"
 export default function Home() {
+    const [search, setSearch]= useState("")
+    const [data, setData]= useState()
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+
+    }
+    const handleChange = ({ target }) => {
+        setSearch({
+          ...search,
+          [target.name]: target.value,
+        });
+      };
   return (
-<Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Search</Form.Label>
-    <Form.Control  placeholder="Enter Home team name" />
-    <Form.Text className="text-muted">
-    If you want to search for a football match enter a team name, for a fight --fighter's name, a race --the race name, etc.
-    </Form.Text>
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+<div className='mainDiv'>
+    <h1>Streams R Us</h1>
+    <form onSubmit={handleSubmit}>
+        <label>
+          Desired Live Event:
+          <input type="text" value={search.searchFor} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+</div>
   )
 }

e4474a5ff74365aa83fe1fe8f61f7b7e2ae0a165
yamodah <yassinedahlek@gmail.com>
2022-04-10 15:10:37 -0400

    minimum viable product - links clickable and renders properly

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,34 +3,42 @@ import axios from 'axios'
 import "./home.css"
 export default function Home() {
     const [search, setSearch]= useState("")
-    const [data, setData]= useState()
+    const [data, setData]= useState({event:"", links:[]})
     const getStreams =async()=>{
-        await axios.get(`http://localhost:5000/all?stream=${search.searchFor}`)
-            .then((res)=>{console.log(res)})
+        await axios.get(`https://yamodahstreams.herokuapp.com/all?stream=${search}`)
+            .then((res)=>{setData(res.data)})
             .catch(console.error)
     }
     const handleSubmit=(e)=>{
         e.preventDefault()
-        console.log(search)
+        // console.log(search)
         getStreams().then(()=>{console.log(data)})
     }
     const handleChange = ({ target }) => {
-        setSearch({
-          ...search,
-          [target.name]: target.value,
-        });
+        setSearch(target.value);
       };
+    
+    const linksHTML = data.links.map((pack,index)=>{
+            if(pack.site ==="ROJA"){
+                // console.log(pack)
+                return pack.streams.map((link, index)=><><a href={link} target="_blank" rel="noopener noreferrer" key={index}>{index}</a><br/></>)
+            }
+
+            return <><a href={pack.streams} target="_blank" rel="noopener noreferrer" key={index}>{pack.site}</a><br/></>
+        })
+    
   return (
 <div className='mainDiv'>
     <h1>Streams R Us</h1>
     <form onSubmit={handleSubmit}>
         <label>
           Desired Live Event:
-          <input type="text" name="searchFor" value={search.searchFor} onChange={handleChange} />
+          <input type="text" name="searchFor" value={search} onChange={handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
-      {/* <h4>{data}</h4> */}
+      {data.event&&<h4>{data.event}</h4>}
+      {data.links.length ?linksHTML:<h3>Your links await you ...</h3>}
 </div>
   )
 }
